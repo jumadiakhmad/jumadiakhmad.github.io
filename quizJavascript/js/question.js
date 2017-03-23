c00f6bf4054699d11c0a55a56073071675eaf4fc
jumadiakhmad <jumadiakhmad2@gmail.com>
2017-03-24 00:28:57 +0700

    Edit FIle question.js

--- quizJavascript/js/question.js
+++ quizJavascript/js/question.js
@@ -6,20 +6,20 @@ var questions=[{
 		"option4": "Kuala Lumpur ",
 		"answer": "2"
 },{		
-		"question": "Apa Ibu Kota Negara Filipina",
+		"question": "Apa Ibu Kota Negara Malaysia",
 		"option1": "Jakarta",
-		"option2": "Manila",
-		"option3": "Bangkok",
-		"option4": "Kuala Lumpur ",
-		"answer": "2"
+		"option2": "Bandar Seri Begawan",
+		"option3": "Kuala Lumpur",
+		"option4": "Hanoi",
+		"answer": "3"
   },{
 	    
-		"question": "Apa Ibu Kota Negara Filipina",
-		"option1": "Jakarta",
-		"option2": "Manila",
-		"option3": "Bangkok",
-		"option4": "Kuala Lumpur ",
-		"answer": "2"
+		"question": "Apa Ibu Kota Negara Italia",
+		"option1": "Paris",
+		"option2": "Munich",
+		"option3": "Milan",
+		"option4": "Roma ",
+		"answer": "4"
 	  
 	  
   },{
