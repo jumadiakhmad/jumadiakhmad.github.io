3b7494968a9078236e92e2f38f71263b2acfa5dd
jumadiakhmad <jumadiakhmad2@gmail.com>
2017-03-24 00:19:48 +0700

    add file question.js and quiz_script.js

---
+++ quizJavascript/js/question.js
@@ -0,0 +1,56 @@
+var questions=[{
+		"question": "Apa Ibu Kota Negara Filipina",
+		"option1": "Jakarta",
+		"option2": "Manila",
+		"option3": "Bangkok",
+		"option4": "Kuala Lumpur ",
+		"answer": "2"
+},{		
+		"question": "Apa Ibu Kota Negara Filipina",
+		"option1": "Jakarta",
+		"option2": "Manila",
+		"option3": "Bangkok",
+		"option4": "Kuala Lumpur ",
+		"answer": "2"
+  },{
+	    
+		"question": "Apa Ibu Kota Negara Filipina",
+		"option1": "Jakarta",
+		"option2": "Manila",
+		"option3": "Bangkok",
+		"option4": "Kuala Lumpur ",
+		"answer": "2"
+	  
+	  
+  },{
+		"question": "Apa Ibu Kota Negara Jepang",
+		"option1": "Tokyo",
+		"option2": "Seoul",
+		"option3": "Beijing",
+		"option4": "Hanoi ",
+		"answer": "1"
+	  
+},{
+		"question": "Apa Ibu Kota Negara Timor Leste",
+		"option1": "Jakarta",
+		"option2": "NTT",
+		"option3": "Dili",
+		"option4": "Singapura",
+		"answer": "3"
+	  
+	  
+},{
+		
+		"question": "Apa Ibu Kota Negara Inggris",
+		"option1": "Manschester",
+		"option2": "Arsenal",
+		"option3": "Chelsea",
+		"option4": "London ",
+		"answer": "2"
+	  
+	}
+	
+	
+	];
+	
+	
